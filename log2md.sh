b188dbfd1b44fbc5650b9b1e9d859233137425eb
Jang Rush <weakish@gmail.com>
2022-08-07 12:17:08 +0000

    Alternatives to homebrew are still alive!
    [MacPorts] supports all the current versions of macOS:
    10.15, 11, and 12.
    
    [Fink] supports the oldest current version of macOS: 10.15.
    In other words, it has not supported M1 yet.
    
    [pkgsrc] built packages against macOS 11 (x86 & M1) and 10.14,
    suitable for users running recent macOS releases.
    
    [MacPorts]: https://trac.macports.org/
    [Fink]: https://www.finkproject.org/
    [pkgsrc]: https://pkgsrc.joyent.com/install-on-osx/

--- log2md.sh
+++ log2md.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git log | sed -r 's/^((commit |Author: |Date: ).+)/\1<br>/g' | sed -r 's/^[ ]{4}//g' > ../weakish.github.com/log/README.md
+git log | sed -r 's/^((commit |Author: ).+)//g' | sed -r 's/^[ ]{4}//g' > ../weakish.github.com/log/README.md
