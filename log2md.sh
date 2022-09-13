ec26d4e90a18463f8e23756a57178f1405959468
Jang Rush <weakish@gmail.com>
2022-09-13 16:05:49 +0000

    The order form of $JD website has a field to fill in the email address to receive the receipt.
    But it turns out they just sent me an email, containing a link to download the receipt from their site.

--- log2md.sh
+++ log2md.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ## TODO add a hash link to every note's unix time
-git log --date=unix | sed -r 's/^((commit |Author: ).+)//g' | sed -r 's/^[ ]{4}//g' > ../weakish.github.com/log/README.md
+git log --date=unix | sed -r 's/^((commit |Author: ).+)//g' | sed -r 's/^[ ]{4}//g' > index.md
