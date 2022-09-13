7772482b6c164dcdfd4d1dd508f10e65c0fbf5db
Jang Rush <weakish@gmail.com>
2022-09-13 16:20:20 +0000

    TypeScript is less apearing to me.
    If I want to write in a dynamic language
    with a powerful gradual type system.
    Then Python ships with it.
    
    If I want a sound type system for JavaScript,
    then there is ReScript.

--- log2md.sh
+++ log2md.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-## TODO add a hash link to every note's unix time
-git log --date=unix | sed -r 's/^((commit |Author: ).+)//g' | sed -r 's/^[ ]{4}//g' > index.md
+## TODO automatic deployment
+git log --date=unix |
+sed -r 's/^((commit |Author: ).+)//g' |
+sed -r 's/^[ ]{4}//g' |
+sed -E 's/^Date:[ ]{3}([0-9]+)$/Date: <a id="\1" href="#\1">\1<\/a>/'> index.md
